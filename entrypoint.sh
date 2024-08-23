96fce243117b228cdd0755ad5fa00bbb00f0f557
Nauman Ahmed <nauman.ahmed@nevron.tv>
2024-08-23 21:51:14 +0500

    Added Exposing Wireguard First

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+# Start wireguard
+wg-quick up wg0
+
 # Start Tailscale in the background
 tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+
 
 # Authenticate and connect to Tailscale
 tailscale up --authkey=${TAILSCALE_AUTH_KEY} --exit-node=${TAILSCALE_EXIT_NODE} --hostname=${TAILSCALE_HOSTNAME} --accept-routes
 
-# Setup wireguard
-wg-quick up wg0
 # Keep the container running
 tail -f /dev/null
